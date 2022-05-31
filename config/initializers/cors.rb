e5e0dd95d4dac7e8b6aaa78b91767d418964b957
funxxfun <23shirasawa@gmail.com>
2022-05-31 10:38:39 +0900

    [change] CORSの設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.production?
-      origins '10-2でデプロイしたURL'
+      origins 'https://rails-nuxt-app-1d7ca.web.app'
     else
       origins 'http://localhost:8080'
     end
